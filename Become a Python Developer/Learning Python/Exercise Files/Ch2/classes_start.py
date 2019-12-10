96182539780e4a20542b0e0eb275858067d21c8a
David Mevorah <mevorahde@hotmail.com>
2019-12-09 21:42:43 -0800

    Update notes for 'Learning Python' class and update .gitignore file.

--- Become a Python Developer/Learning Python/Exercise Files/Ch2/classes_start.py
+++ Become a Python Developer/Learning Python/Exercise Files/Ch2/classes_start.py
@@ -3,8 +3,32 @@
 #
 
 
-def main():
+class myClass():
+    def method1(self):
+        print("my_class method1")
 
-  
+    def method2(self, some_string):
+        print("my_class method2 " + some_string)
+
+
+class secondClass(myClass):
+  def method1(self):
+    myClass.method1(self)
+    print("secondClass method1")
+
+  def method2(self):
+    print ("secondClass method2")
+
+
+def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+
+    c2 = secondClass()
+    c2.method1()
+    c2.method2()
+
+
 if __name__ == "__main__":
-  main()
+    main()
